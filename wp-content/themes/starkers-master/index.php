875ca56707e7921659ca8c125779942256521719
Sophia Dengo <sdengo@gmail.com>
2014-05-30 13:41:22 -0700

    first commit

---
+++ wp-content/themes/starkers-master/index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The main template file
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file 
+ *
+ * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
+ *
+ * @package 	WordPress
+ * @subpackage 	Starkers
+ * @since 		Starkers 4.0
+ */
+?>
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+
+<?php if ( have_posts() ): ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+	<?php if ( in_category( 'intro' )) { ?>
+	
+	
+		<article class='intro'>
+			<h2><a href="<?php echo get_post_meta( get_the_ID(), 'link', true ) ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			<?php the_content(); ?>
+		
+		</article>
+	
+	
+	<?php } ?>
+	
+<?php endwhile; ?>
+
+
+<?php else: ?>
+<h2>No posts to display</h2>
+<?php endif; ?>
+
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer') ); ?>
