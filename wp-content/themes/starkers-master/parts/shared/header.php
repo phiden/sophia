0f0693bbdf09b4074ad163ff317209afd85490b4
Sophia Dengo <sdengo@gmail.com>
2014-06-14 12:27:14 -0500

    background colors and post development

--- wp-content/themes/starkers-master/parts/shared/header.php
+++ wp-content/themes/starkers-master/parts/shared/header.php
@@ -1,15 +1,16 @@
 <header>
 	<!--<h1>"><?php bloginfo( 'name' ); ?></a></h1>-->
 	
-	
+	<li class="main-nav-logo"><a href="<?php echo home_url(); ?>">sophia <img src="http://localhost:8888/sophia/wp-content/themes/starkers-master/img/sd-medallion-pink.png"/> dengo&nbsp;&nbsp;</a></li>
 	<nav>
 	
 		<li class="main-nav"><a href="developer">front-end developer.</a></li>
 		<li class="main-nav"><a href="designer">designer.</a></li>
 		<li class="main-nav"><a href="maker">maker.</a></li>
-		<li class="main-nav"><a href="<?php echo home_url(); ?>"><img src="http://localhost:8888/sophia/wp-content/themes/starkers-master/img/sd-medallion-pink.png"/></a></li>
+		
 		<li class="main-nav"><a href="museums">museum nerd.</a></li>
 		<li class="main-nav"><a href="writer">writer.</a></li>
+		<li class="main-nav"><a href="writer">r&eacute;sum&eacute;.</a></li>
 		<li class="main-nav"><a href="#contact">contact me!</a></li>
 		
 	</nav>
