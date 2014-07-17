fb98ff32d1946eacf5be4497719b5e7644ce0853
Sophia Dengo <sdengo@gmail.com>
2014-07-17 11:37:13 -0700

    Added header image and tweaked header CSS

--- wp-content/themes/starkers-master/parts/shared/header.php
+++ wp-content/themes/starkers-master/parts/shared/header.php
@@ -1,14 +1,13 @@
 <header>
 	<!--<h1>"><?php bloginfo( 'name' ); ?></a></h1>-->
 	
-	<li class="main-nav-logo"><a href="<?php echo home_url(); ?>"><img src="http://localhost:8888/sophia/wp-content/themes/starkers-master/img/signature.png"/></a></li>
+	<li class="main-nav-logo"><a href="<?php echo home_url(); ?>"><img src="http://localhost:8888/sophia/wp-content/themes/starkers-master/img/signature-white.png"/></a></li>
 	<nav>
 	
 		<li class="main-nav"><a href="developer">front-end developer.</a></li>
 		<li class="main-nav"><a href="designer">designer.</a></li>
 		<li class="main-nav"><a href="maker">maker.</a></li>
 		
-		<li class="main-nav"><a href="museums">museum nerd.</a></li>
 		<li class="main-nav"><a href="resume">r&eacute;sum&eacute;.</a></li>
 		<li class="main-nav"><a href="#contact">contact me!</a></li>
 		
