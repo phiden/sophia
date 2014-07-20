09e3645bde557f6d4536859153860d7356d42e21
Sophia Dengo <sdengo@gmail.com>
2014-07-20 10:11:48 -0700

    added in freetile.js to handle responsiveness on designer page, plus a quickie replace-with-freekitten function to handle broken images in dev

--- wp-content/themes/starkers-master/js/site.js
+++ wp-content/themes/starkers-master/js/site.js
@@ -1,10 +1,22 @@
+jQuery(document).ready(function($) {
 
-	jQuery(document).ready(function($) {
-
-		// Your JavaScript goes here
-		$("#user-info").click(function(){ 
+	// Your JavaScript goes here
+	$("#user-info-dismiss").click(function(){ 
 			
-			$(this).slideUp();
-		})
+		$("#user-info").slideUp();
+	})
+
+	$("#designer-page article").freetile({
+		
+		animate:true
 	});
+	
+	
+	//quickie to replace broken images with placekittens for dev purposes.
+	$('.work-card img').each(function() {
+		
+		$(this).attr('src', 'http://placekitten.com/g/600/600/');
+	})
+	
+});
 
--- wp-content/themes/starkers-master/parts/shared/html-footer.php
+++ wp-content/themes/starkers-master/parts/shared/html-footer.php
@@ -1,4 +1,7 @@
 
 	<?php wp_footer(); ?>
 	</body>
+	
+	<script src="http://localhost:8888/sophia/wp-content/themes/starkers-master/js/jquery.freetile.min.js" type="text/javascript"></script>
+	
 </html>
