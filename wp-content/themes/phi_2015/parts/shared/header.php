ddef84efa0d8eee49662a5ac85ad31dc643bbf8f
phiden <sdengo@codeforamerica.org>
2015-09-03 12:05:56 -0700

    changed header entirely

--- wp-content/themes/phi_2015/parts/shared/header.php
+++ wp-content/themes/phi_2015/parts/shared/header.php
@@ -1,20 +1,14 @@
 <header>
 	
-	<div id='name-bar'>
-		<h1 class='f_left'><a href="<?php echo home_url(); ?>">sophia dengo</a></h1>
-		
-		<nav class='f_right'>
-			<li class="main-nav"><a href="#developer">front-end developer</a></li>
-			<li class="main-nav"><a href="#designer">designer</a></li>
-			<li class="main-nav"><a href="#maker">maker</a></li>	
-			<li class="main-nav"><a href="#resume">r&eacute;sum&eacute;</a></li>
-			<li class="main-nav"><a href="http://github.com/phiden">on Github</a></li>
-			<li class="main-nav"><a href="#contact">contact me</a></li>
-		</nav>
+	<div class='outer'>
+			<div class='inner' id='header-content'>
+				<a href='#'>
+					<h1>sophia dengo</h1>
+					<h5 class='light no-margin'>maker of physical &amp; digital things.</h5>
+				</a>
+			</div>
 	</div>
-	
-	<div id="mobile-nav-trigger" class='hidden'><a href=#mobile-menu>MENU</a></div>
-	
 </header>
 
 <section id='global-container'> <!-- closes in footer -->
+
